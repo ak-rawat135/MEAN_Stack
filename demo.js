3db91626b7230df29a3a1d686b567cac2d85dab8
Arpit Kumar <kumararpit135@gmail.com>
2026-07-07 22:52:24 +0530

    updated no need extra stuff 😂😂

--- demo.js
+++ demo.js
@@ -1,7 +1,38 @@
-let arr=["HI", "There", "HOW ARE you"];
-for (let ar of arr){
-    console.log(ar)
-}
-for(let x=0;x<=arr.length;x++){
-    console.log(arr[x])
-}
+// let arr=["HI", "There", "HOW ARE you"];
+// for (let ar of arr){
+//     console.log(ar)
+// }
+// for(let x=0;x<=arr.length;x++){
+//     console.log(arr[x])
+// }
+
+
+/*
+no need bin 
+  public
+  view or anything 
+
+
+
+
+just create a app.js 
+do npm init 
+set app.js as main
+when packege.json load 
+install nodemon => npm install nodemon 
+
+go this packege.json 
+
+in script 
+ "scripts": {
+    "test": "echo \"Error: no test specified\" && exit 1",
+    "start": "nodemon app.js",
+    "start-server": "node app.js"
+  }, 
+
+  add this start and start server line in code so basicly node mon will one things when ever you 
+  change anythings in the code you don't need to close server then save then restart you 
+  save save it will automaticly updated in the server as well like live one 
+  second line read what it's doing */
+
+  
